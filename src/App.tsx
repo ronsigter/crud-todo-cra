d56fce807f32abb0543e5da9136e8b4a8d73d1c1
Chester Mendoza <ronchester.mendoza@gmail.com>
2021-07-05 10:10:40 +0800

    setups react-query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import Todos from 'containers/Todos'
 import Todo from 'containers/Todo'
+
+const queryClient = new QueryClient()
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route path='/'>
-          <Todos />
-        </Route>
-        <Route path='/todo/:id'>
-          <Todo />
-        </Route>
+        <QueryClientProvider client={queryClient}>
+          <Route path='/'>
+            <Todos />
+          </Route>
+          <Route path='/todo/:id'>
+            <Todo />
+          </Route>
+        </QueryClientProvider>
       </Switch>
     </Router>
   )
