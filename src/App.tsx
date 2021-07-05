e3c1819d7426640c7354e5dac5eafa0acd081a42
Chester Mendoza <ronchester.mendoza@gmail.com>
2021-07-05 10:05:06 +0800

    adds containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
-function App() {
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Todos from 'containers/Todos'
+import Todo from 'containers/Todo'
+
+const App: React.FC = () => {
   return (
-    <section className='App h-screen w-full flex justify-center items-center bg-green-500'>
-      <div className='w-full max-w-md bg-gray-800'>
-        <form action='' className=' bg-white shadow-md rounded px-8 py-8 pt-8'>
-          <div className='px-4 pb-4'>
-            <label htmlFor='email' className='text-sm block font-bold  pb-2'>
-              EMAIL ADDRESS
-            </label>
-            <input
-              type='email'
-              name='email'
-              id=''
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 '
-              placeholder='Johnbull@example.com'
-            />
-          </div>
-          <div className='px-4 pb-4'>
-            <label htmlFor='password' className='text-sm block font-bold pb-2'>
-              PASSWORD
-            </label>
-            <input
-              type='password'
-              name='email'
-              id=''
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300'
-              placeholder='Enter your password'
-            />
-          </div>
-          <div>
-            <button
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-              type='button'
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
-      </div>
-    </section>
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Todos />
+        </Route>
+        <Route path='/todo/:id'>
+          <Todo />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
