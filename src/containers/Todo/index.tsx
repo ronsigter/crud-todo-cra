2974267e338be71cbb51b15e1ed8d503cd7f2624
Chester Mendoza <ronchester.mendoza@gmail.com>
2021-07-07 01:39:24 +0800

    adds delete on todo

--- src/containers/Todo/index.tsx
+++ src/containers/Todo/index.tsx
@@ -1,15 +1,25 @@
-import { Link, useParams } from 'react-router-dom'
-import { useGetTodo, useUpdateTodo } from 'hooks'
+import { Link, useParams, useHistory } from 'react-router-dom'
+import { useToasts } from 'react-toast-notifications'
+import { useDeleteTodo, useGetTodo, useUpdateTodo } from 'hooks'
 import Loader from 'containers/Todo/Loader'
 import ToggleButton from 'component/ToggelButton'
 
 const Todo: React.FC = () => {
+  const history = useHistory()
   const { id } = useParams<{ id: string }>()
+  const { addToast } = useToasts()
   const { todo, status } = useGetTodo(id)
   const { updateTodo } = useUpdateTodo()
+  const { deleteTodo } = useDeleteTodo()
 
   const handleOnChangeStatus = (status: boolean) => {
     updateTodo({ ...todo, isActive: status })
+  }
+
+  const handleOnDelete = () => {
+    deleteTodo([todo.id])
+    addToast('Activity Added Successfully', { appearance: 'success' })
+    history.push('/')
   }
 
   return (
@@ -18,6 +28,9 @@ const Todo: React.FC = () => {
       <Link to={`/edit/${todo?.id}`}>
         <p>Edit</p>
       </Link>
+      <div>
+        <button onClick={handleOnDelete}>delete</button>
+      </div>
       <ToggleButton
         isActive={todo?.isActive || false}
         onChange={handleOnChangeStatus}
