f923022f2d64dfb17fa63022f5ee6863983638a6
Chester Mendoza <ronchester.mendoza@gmail.com>
2021-07-06 21:54:38 +0800

    creates create todo form

--- src/containers/CreateTodo/index.tsx
+++ src/containers/CreateTodo/index.tsx
@@ -1,5 +1,19 @@
 const CreateTodo = () => {
-  return <div>CreateTodo</div>
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor='title'>title:</label>
+          <input type='text' id='title' name='title' />
+        </div>
+        <div>
+          <label htmlFor='description'>description:</label>
+          <input type='text' id='description' name='description' />
+        </div>
+        <button type='submit'>Add Activity</button>
+      </form>
+    </div>
+  )
 }
 
 export default CreateTodo
